1394930f31d8b32d20ade02033871d5f1063ded7
Deven khichar <95070492+devenkhichar@users.noreply.github.com>
2022-02-25 22:11:13 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,34 @@
+side = int(input('side'))
+#Take the user input for side value
+peri_sq = 4 * side
+area_sq = side ** 2
+area_cube = 6 * side ** 2
+vol_cube = side ** 3
+print('''
+The perimeter of square is : ''', peri_sq, '''
+The area of square is : ''', area_sq, '''
+The area of cube is : ''', area_cube, '''
+The volume of cube is : ''', vol_cube)
+#write the variable name for corresponding,
+#write the variable name for corresponding,
+#rite the variable name for corresponding)
+radius = int(input('radius'))
+#Take the user input for radius value
+peri_cir = (2 * (22 / 7)) * radius
+area_cir = (22 / 7) * radius ** 2
+area_sph = (4 * (22 / 7)) * radius ** 2
+vol_sph = ((4 / 3) * (22 / 7)) * radius ** 3
+print('''
+The perimeter of circle is : ''', peri_cir, '''
+The area of circle is : ''', area_cir, '''
+The area of sphere is : ''', area_sph, '''
+The volume of sphere is : ''', vol_sph)
+#write the variable name for corresponding,
+#write the variable name for corresponding,
+#write the variable name for corresponding,
+#rite the variable name for corresponding)
+r = int(input('cone'))
+area_cone = ((22 / 7) * side) * r
+print('''
+The area of cone is : ''', area_cone)
+#write the variable name for corresponding)
